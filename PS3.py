83a3934290e6be762ff145caf79649ee59579497
mw5044 <michal.walas@hurra.com>
2014-09-16 18:21:47 +0200

    save works

---
+++ PS3.py
@@ -0,0 +1,26 @@
+def radiationExposure(start, stop, step):
+    '''
+    Computes and returns the amount of radiation exposed
+    to between the start and stop times. Calls the 
+    function f (defined for you in the grading script)
+    to obtain the value of the function at any point.
+ 
+    start: integer, the time at which exposure begins
+    stop: integer, the time at which exposure ends
+    step: float, the width of each rectangle. You can assume that
+      the step size will always partition the space evenly.
+
+    returns: float, the amount of radiation exposed to 
+      between start and stop times.
+    '''
+    # FILL IN YOUR CODE HERE...
+
+
+radiationExposure(0, 5, 1)
+#39.10318784326239
+radiationExposure(5, 11, 1)
+#22.94241041057671
+radiationExposure(0, 11, 1)
+#62.0455982538
+radiationExposure(40, 100, 1.5)
+#0.434612356115
