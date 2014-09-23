39a269fb7ba5cc6832009e0542ac8808cfad9827
mw5044 <michal.walas@hurra.com>
2014-09-23 14:39:08 +0200

    problem set 4 - implemented function compChooseWord

--- week_4/ProblemSet4/ps4b.py
+++ week_4/ProblemSet4/ps4b.py
@@ -23,25 +23,25 @@ def compChooseWord(hand, wordList, n):
 
     returns: string or None
     """
-    # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Create a new variable to store the maximum score seen so far (initially 0)
-
+    maximumScore = 0
     # Create a new variable to store the best word seen so far (initially None)  
-
+    bestWord = None
     # For each word in the wordList
-
+    for word in wordList:
         # If you can construct the word from your hand
         # (hint: you can use isValidWord, or - since you don't really need to test if the word is in the wordList - you can make a similar function that omits that test)
-
+        if isValidWord(word, hand, wordList):
             # Find out how much making that word is worth
-
+            score = getWordScore(word, n)
             # If the score for that word is higher than your best score
-
+            if score > maximumScore:
                 # Update your best score, and best word accordingly
-
+                maximumScore = score
+                bestWord = word
 
     # return the best word you found.
-
+    return bestWord
 
 #
 # Problem #7: Computer plays a hand
@@ -105,5 +105,5 @@ def playGame(wordList):
 if __name__ == '__main__':
     wordList = loadWords()
     playGame(wordList)
-
-
+print 'aaaaaa'
+print compChooseWord({'a': 1, 'p': 2, 's': 1, 'e': 1, 'l': 1}, wordList, 6)
